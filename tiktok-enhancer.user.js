146252f7a39f40080e7ee5fe7512e183e555a56d
n30liberal <118079899+n30liberal@users.noreply.github.com>
2023-02-11 04:55:41 -0600

    Update tiktok-enhancer.user.js
    Fixed the slippery scroll that would happen sometimes. Just needed to make sure the jiggles happened less often.

--- tiktok-enhancer.user.js
+++ tiktok-enhancer.user.js
@@ -3,9 +3,9 @@
 // @namespace   https://www.tiktok.com/
 // @include     https://www.tiktok.com/@*
 // @author ne0liberal
-// @updateURL https://github.com/n30liberal/tiktok-browser-scripts/raw/main/tiktok-enhancer.user.js
-// @downloadURL https://github.com/n30liberal/tiktok-browser-scripts/raw/main/tiktok-enhancer.user.js
-// @version     1.0.3
+// @updateURL https://github.com/n30liberal/random-userscripts/raw/main/tiktok-enhancer.user.js
+// @downloadURL https://github.com/n30liberal/random-userscripts/raw/main/tiktok-enhancer.user.js
+// @version     1.0.5
 // @grant       none
 // ==/UserScript==
 
