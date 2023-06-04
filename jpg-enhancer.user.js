371a2e96732271f960cb6e467b3c5d259b202d72
n30liberal <118079899+n30liberal@users.noreply.github.com>
2023-06-04 02:03:17 -0500

    Add files via upload

---
+++ jpg-enhancer.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Redirect to og:image URL.
+// @namespace    https://jpg.pet/
+// @version      1.0
+// @description  Redirects the page to the URL specified in the og:image meta tag
+// @match        https://jpg.pet/*
+// @updateURL https://github.com/n30liberal/random-userscripts/raw/main/jpg-enhancer.user.js
+// @downloadURL https://github.com/n30liberal/random-userscripts/raw/main/jpg-enhancer.user.js
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Find the og:image meta tag
+    const ogImageMeta = document.querySelector('meta[property="og:image"]');
+    if (ogImageMeta) {
+        const imageUrl = ogImageMeta.getAttribute('content');
+        if (imageUrl) {
+            // Redirect to the image URL
+            window.location.href = imageUrl;
+        }
+    }
+})();
