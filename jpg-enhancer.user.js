7dc6e526b851a06c59d70d4d7069dedafc46a908
n30liberal <118079899+n30liberal@users.noreply.github.com>
2023-06-04 03:53:50 -0500

    Update jpg-enhancer.user.js

--- jpg-enhancer.user.js
+++ jpg-enhancer.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Redirect to og:image URL (excluding login URLs)
+// @name         Redirect to og:image URL
 // @namespace    https://jpg.pet/
 // @author       ne0liberal
-// @version      1.5
-// @description  Redirects the page to the URL specified in the og:image meta tag, excluding login URLs
+// @version      1.6
+// @description  Redirects the page to the URL specified in the og:image meta tag, excluding login and upload URLs
 // @match        https://*.jpg.pet/*
 // @match        https://*.pixl.li/*
 // @match        https://*.jpg.church/*
@@ -15,10 +15,21 @@
 (function () {
     'use strict';
 
-    // Check if the source URL contains the word "login"
+    // Check if the source URL or og:image content contains certain keywords
     function shouldRedirect() {
         const sourceUrl = window.location.href;
-        return !/login/i.test(sourceUrl);
+        const ogImageMeta = document.querySelector('meta[property="og:image"]');
+        const imageUrl = ogImageMeta ? ogImageMeta.getAttribute('content') : null;
+
+        const keywords = ['login', 'upload', 'system'];
+
+        for (let i = 0; i < keywords.length; i++) {
+            if (sourceUrl.toLowerCase().includes(keywords[i]) || (imageUrl && imageUrl.toLowerCase().includes(keywords[i]))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     // Find the og:image meta tag
