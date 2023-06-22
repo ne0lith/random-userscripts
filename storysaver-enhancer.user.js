ec99457feda4c4558eafe7de0057f6e110ebbb5b
n30liberal <118079899+n30liberal@users.noreply.github.com>
2023-06-22 14:44:51 -0500

    Update storysaver-enhancer.user.js

--- storysaver-enhancer.user.js
+++ storysaver-enhancer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram CDN URL Extractor and File Downloader via StorySaver
 // @namespace    your-namespace
-// @version      1.5
+// @version      1.7
 // @author ne0liberal
 // @description  Extracts CDN URLs from Instagram and saves files with the username as a prefix
 // @match        https://www.storysaver.net/*
@@ -50,17 +50,32 @@
         }
 
         // Save each file with the username as prefix
-        var delay = 1000; // Delay in milliseconds
+        var delay = 250; // Delay in milliseconds
         var index = 0;
         var downloadedFilenames = new Set(); // Track downloaded filenames
+
+        // Load download history from localStorage
+        var downloadHistory = JSON.parse(localStorage.getItem("downloadHistory")) || {};
+
         cdnUrls.forEach(function(url) {
             var filename = url.split("/").pop().split("?")[0];
             filename = username + "-" + filename;
 
             // Check if the file has already been downloaded
-            if (!downloadedFilenames.has(filename)) {
+            if (!downloadedFilenames.has(filename) && !downloadHistory[filename]) {
                 setTimeout(function() {
                     saveFile(url, filename);
+
+                    // Update download history
+                    downloadHistory[filename] = true;
+                    localStorage.setItem("downloadHistory", JSON.stringify(downloadHistory));
+
+                    // Check if all downloads are finished
+                    if (downloadedFilenames.size === cdnUrls.size - 1) {
+                        var totalDownloads = cdnUrls.size - 1;
+                        var message = "Downloads Finished (" + totalDownloads + "/" + totalDownloads + ")";
+                        alert(message);
+                    }
                 }, delay * index);
 
                 downloadedFilenames.add(filename); // Add filename to the set of downloaded filenames
