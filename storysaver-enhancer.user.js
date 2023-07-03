1b7e609eead2c25e439825450c37455f98b1f7f9
n30liberal <118079899+n30liberal@users.noreply.github.com>
2023-07-03 14:46:31 -0500

    Update storysaver-enhancer.user.js

--- storysaver-enhancer.user.js
+++ storysaver-enhancer.user.js
@@ -1,17 +1,25 @@
 // ==UserScript==
 // @name         Instagram CDN URL Extractor and File Downloader via StorySaver
 // @namespace    your-namespace
-// @version      3.3
+// @version      4.0
 // @author       ne0liberal
 // @description  Extracts CDN URLs from Instagram and saves files with the username as a prefix
 // @match        https://www.storysaver.net/*
 // @updateURL    https://github.com/n30liberal/random-userscripts/raw/main/storysaver-enhancer.user.js
 // @downloadURL  https://github.com/n30liberal/random-userscripts/raw/main/storysaver-enhancer.user.js
 // @grant        GM_xmlhttpRequest
-// @connect      cdninstagram.com fbcdn.net instagram.frao1-1.fna.fbcdn.net fna.fbcdn.net scontent-mrs2-2.cdninstagram.com
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @connect      scontent-ams2-1.cdninstagram.com
+// @connect      scontent-fra3-1.cdninstagram.com
+// @connect      scontent-fra3-2.cdninstagram.com
+// @connect      scontent-fra5-1.cdninstagram.com
+// @connect      scontent-gru2-2.cdninstagram.com
+// @connect      scontent-mrs2-2.cdninstagram.com
+// @connect      scontent-mia3-2.cdninstagram.com
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     function saveFile(url, filename) {
@@ -19,7 +27,7 @@
             method: "GET",
             url: url,
             responseType: "blob",
-            onload: function(response) {
+            onload: function (response) {
                 var a = document.createElement("a");
                 a.href = window.URL.createObjectURL(response.response);
                 a.download = filename;
@@ -35,11 +43,11 @@
         var cdnUrls = new Set();
         var username = '';
 
-        // Extract CDN URLs with _nc_ht query param
+        // Extract CDN URLs with _nc_ht= query param
         var links = document.getElementsByTagName("a");
         for (var i = 0; i < links.length; i++) {
             var link = links[i].href;
-            if ((link.includes("cdninstagram.com") || link.includes("fbcdn.net")) && link.includes("&_nc_ht")) {
+            if (link.includes("&_nc_ht=")) {
                 cdnUrls.add(link);
             }
         }
@@ -50,36 +58,48 @@
             username = usernameElement.innerText.trim().toLowerCase();
         }
 
+        // Extract story count
+        var storyCount = 0;
+        var storyCountElement = document.querySelector("div.storycount");
+        if (storyCountElement) {
+            var storyCountText = storyCountElement.innerText.trim();
+            storyCount = parseInt(storyCountText.split(" ")[0]);
+        }
+
         // Save each file with the username as prefix
-        var delay = 250; // Delay in milliseconds
+        var delay = 150; // Delay in milliseconds
         var index = 0;
         var downloadedFilenames = new Set(); // Track downloaded filenames
 
         // Load download history from localStorage
+        // MAJOR ISSUE WITH USING HISTORY TO TRACK DOWNLOADED FILES
+        // IF YOU DIDNT HAVE THE CDN URL IN YOUR @CONNECT TAGS, IT WILL NOT DOWNLOAD
+        // BUT STILL MARK IT AS DOWNLOADED IN THE HISTORY
+        // so to try try the download, just append X to the downloadHistoryStorySaver getItem key
+        // so it will use a new history
         var downloadHistoryStorySaver = JSON.parse(localStorage.getItem("downloadHistoryStorySaver")) || {};
 
 
-
         var from_domains = new Set();
-        cdnUrls.forEach(function(url) {
+        cdnUrls.forEach(function (url) {
             var domain = url.split("/")[2];
             from_domains.add(domain);
         });
 
-        from_domains.forEach(function(domain) {
-            console.log("Downloading from: ", domain);
+        from_domains.forEach(function (domain) {
+            console.log("Downloading from:", domain);
         });
-        console.log("Username: ", username);
-        console.log("Downloading: ", cdnUrls.size, " files");
-        console.log("Be sure to whitelist or add to @connect these domains in tampermonkey, or the script will not work!");
+        console.log("Username:", username);
+        console.log("Downloading:", storyCount, "files");
+        console.log("If you are seeing console errors after this,");
+        console.log("be sure to add these domains to the @connect tag in this userscript, and the User domain whitelist.");
 
-        cdnUrls.forEach(function(url) {
+        cdnUrls.forEach(function (url) {
             var filename = url.split("/").pop().split("?")[0];
             filename = username + "-" + filename;
 
-            // Check if the file has already been downloaded
             if (!downloadedFilenames.has(filename) && !downloadHistoryStorySaver[filename]) {
-                setTimeout(function() {
+                setTimeout(function () {
                     saveFile(url, filename);
 
                     // Update download history
@@ -109,4 +129,5 @@
     document.body.appendChild(box);
 
     console.log("Script loaded"); // Log that the script has loaded
+
 })();
