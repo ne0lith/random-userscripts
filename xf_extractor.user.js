836f271b5906e46474e3be7219161248003b9c26
ne0lith <118079899+ne0lith@users.noreply.github.com>
2023-12-19 02:55:56 -0600

    Update and rename sc_extractor.user.js to xf_extractor.user.js

--- xf_extractor.user.js
+++ xf_extractor.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         URL Extractor
-// @version      1.5
+// @version      1.0
 // @description  Extracts URLs with specified substrings.
 // @author       neolith
 // @match        https://simpcity.su/threads/*
-// @updateURL    https://github.com/ne0lith/random-userscripts/raw/main/sc_extractor.user.js
-// @downloadURL  https://github.com/ne0lith/random-userscripts/raw/main/sc_extractor.user.js
+// @updateURL    https://github.com/ne0lith/random-userscripts/raw/main/xf_extractor.user.js
+// @downloadURL  https://github.com/ne0lith/random-userscripts/raw/main/xf_extractor.user.js
 // @grant        GM_setClipboard
 // ==/UserScript==
 
@@ -63,27 +63,40 @@
         scrollToBottom();
     }
 
+    var containerDiv = document.createElement('div');
+    containerDiv.style.position = 'fixed';
+    containerDiv.style.top = '218px';
+    containerDiv.style.left = '30px';
+    containerDiv.style.width = '280px';
+    containerDiv.style.height = '70px';
+    containerDiv.style.background = '#272727';
+    containerDiv.style.cursor = 'pointer';
+    containerDiv.style.zIndex = '9998';
+    containerDiv.style.borderRadius = '10px';
+    containerDiv.style.border = '1px solid #414141';
+
     var label = document.createElement('label');
     label.textContent = 'Domains to extract:';
-    label.style.position = 'fixed';
-    label.style.top = '97px';
-    label.style.left = '70px';
+    label.style.position = 'absolute';
+    label.style.top = '7px';
+    label.style.left = '10px';
     label.style.zIndex = '9999';
+    label.style.color = '#d3d3d3';
 
     var inputBox = document.createElement('input');
     inputBox.type = 'text';
-    inputBox.style.position = 'fixed';
-    inputBox.style.top = '123px';
-    inputBox.style.left = '70px';
+    inputBox.style.position = 'absolute';
+    inputBox.style.top = '33px';
+    inputBox.style.left = '10px';
     inputBox.style.width = '200px';
     inputBox.placeholder = 'Add substrings (comma-separated)';
     inputBox.style.zIndex = '9999';
-    inputBox.value = defaultSubstrings.join(', ');
+    inputBox.value = defaultSubstrings.join(', ')
 
     var box = document.createElement('div');
-    box.style.position = 'fixed';
-    box.style.top = '100px';
-    box.style.left = '10px';
+    box.style.position = 'absolute';
+    box.style.top = '10px';
+    box.style.left = '220px';
     box.style.width = '50px';
     box.style.height = '50px';
     box.style.background = 'red';
@@ -91,9 +104,11 @@
     box.style.zIndex = '9999';
     box.title = 'Click to extract URLs with specified substrings';
 
-    document.body.appendChild(label);
-    document.body.appendChild(inputBox);
-    document.body.appendChild(box);
+    containerDiv.appendChild(label);
+    containerDiv.appendChild(inputBox);
+    containerDiv.appendChild(box);
+
+    document.body.appendChild(containerDiv);
 
     box.addEventListener('click', function () {
         substringsToSearch = inputBox.value.split(',').map(substring => substring.trim()).filter(Boolean);
